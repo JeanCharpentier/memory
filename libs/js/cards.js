a40eba2df4426b772920319be4744ed7724a2f20
JeanCharpentier <jeancharpentier@protonmail.com>
2022-10-09 12:15:24 +0200

    Commentaires du JS

--- libs/js/cards.js
+++ libs/js/cards.js
@@ -7,53 +7,54 @@
                                  
 ****************************************************
 On crée les cartes sous forme d'objet pour gérer leur affichage, leur état et la "couleur" de chacune.
-Afin de limiter les chargements d'images, le choix à été fait d'utiliser une seule spritesheet découpée dans la méthode drawImage et d'afficher les cartes sous forme de canvas et non en tant qu'image.
+Afin de limiter les chargements d'images, le choix à été fait d'utiliser une seule spritesheet découpée dans la méthode drawImage
+et d'afficher les cartes sous forme de canvas et non en tant qu'image.
 ****************************************************
 */
 
 class Card {
     constructor(pFruit) {
-        this.x = 0;
-        this.y = 0;  
         this.ctx;
-        this.fruit = pFruit; // Le "fruit" est défini lors de la création de la carte par le "sac"
+        this.fruit = pFruit; // Le "fruit" est défini sous forme d'entier lors de la création de la carte par le "sac"
         this.state = 0;
     }
 
     drawCard(pImage) {
-        switch(this.state) {
-            case 0:
-                // Paire non trouvée
-                this.ctx.fillStyle = "#8F5350";
-                this.ctx.fillRect(0,0,100,100);
+        switch(this.state) { // Selon l'état de la carte, on la dessine différement
+            case 0: // Paire non trouvée
+                this.ctx.fillStyle = "#8F5350"; // Définision de la couleur qui va être dessinée
+                this.ctx.fillRect(0,0,100,100); // Remplissage du canvas avec la couleur choisie
                 break;
-            case 1:
-                // La carte retournée
+            case 1:// La carte retournée                
+                // On dessine l'image en la découpant virtuellement selon le numéro de fruit de la carte
                 this.ctx.drawImage(pImage, 0, this.fruit * 100, 100, 100, 0, 0, 100, 100);
                 break;
-            case 2:
-                // Paire trouvée
-                this.ctx.fillStyle = "#DBCFE6";
-                this.ctx.fillRect(0,0,100,100);
+            case 2:// Paire trouvée
+                this.ctx.fillStyle = "#DBCFE6"; // Définision de la couleur qui va être dessinée
+                this.ctx.fillRect(0,0,100,100); // Remplissage du canvas avec la couleur choisie
                 break;
             default:
                 break;
         }     
     }
 
-    getFruit() { // On peut lire le fruit de chaque carte depuis le main.js
+    // On peut lire le fruit de chaque carte depuis le main.js
+    getFruit() {
         return this.fruit;
     }
     
-    setCtx(pCtx) { // On renseigne le contexte de chaque carte une fois que celle-ci est créée dans la page HTML
+    // On renseigne le contexte de chaque carte une fois que celle-ci est créée dans la page HTML
+    setCtx(pCtx) { 
         this.ctx = pCtx;
     }
 
-    setState(pState) { // On modifie l'état de la carte lorsqu'on la retourne
+    // On modifie l'état de la carte lorsqu'on la retourne
+    setState(pState) {
         this.state = pState;
     }
 
-    getState() { // On regarde l'état de la carte pour savoir si elle peut être retournée ou non
+    // On regarde l'état de la carte pour savoir si elle peut être retournée ou non
+    getState() {
         return this.state;
     }
 }
--- main.js
+++ main.js
@@ -1,78 +1,128 @@
+/*
+   ___  ____ _____ ___ ___  _   _ ____  
+  / _ \|  _ \_   _|_ _/ _ \| \ | / ___| 
+ | | | | |_) || |  | | | | |  \| \___ \ 
+ | |_| |  __/ | |  | | |_| | |\  |___) |
+  \___/|_|    |_| |___\___/|_| \_|____/ 
+*/
+
+const paires = 18; // Nombre de paires à trouver (donc cartes = paires * 2)
+const maxTime = 10; // Temps pour résoudre le Memory
+var currentTime = maxTime; // Initialisation du compteur de temps
+const timeToCheck = 1; // Temps de regard sur les paires, en milliseconde
+
+/*
+  __  __    _    ___ _   _ 
+ |  \/  |  / \  |_ _| \ | |
+ | |\/| | / _ \  | ||  \| |
+ | |  | |/ ___ \ | || |\  |
+ |_|  |_/_/   \_\___|_| \_|
+*/
+
 const grille = document.querySelector('.grille');
 const timerBar = document.querySelector('.timer');
 const timerText = document.querySelector('.timerValue');
 const timerBarWidth = timerBar.clientWidth;
 
 const bag = new Bag();
-const paires = 18;
-var pairesTrouvees = 0
 
-const maxTime = 180;
-var currentTime = maxTime;
-var canPlay = true;
+var pairesTrouvees = 0;
 
-const timeToCheck = 1; // Temps de regard sur les paires
+var canPlay = true; // Booléen pour savoir si le joueur peut encore retourner des cartes (victoire ou défaite)
 
-var selectedCards = [];
-var Cards = [];
+var Cards = []; // Les cartes actuellement sur le plateau, rangées dans leur ordre d'apparition
+var selectedCards = []; // Les cartes actuellement sélectionnées
+
 
 var image = new Image();
-image.src = "./assets/images/cards.png";
+image.src = "./assets/images/cards.png"; // L'object image est un ensemble de plus petites images, "découpées" lors de la création des objets cartes
 
-var blankCard = new Image();
-blankCard.src = "./assets/images/blank.png";
-
-image.onload = function() {
+image.onload = function() { // Quand l'image des cartes est chargée, le script se lance
 
     bag.fillBag(paires); // Génération du sac dans lequel on va piocher les cartes aléatoirement
 
     for(var i = 0; i < paires * 2;i++) {
+
+        // Création des emplacements ou seront affichées les cartes
         var card = document.createElement('canvas');
         card.setAttribute('width',100);
         card.setAttribute('height',100);
-        
-        card.addEventListener('click',flip);
 
-        var cardId = rnd(0,bag.lstCards.length-1); // Numéro de la carte dans le sac
-
-        
-        var ctx = card.getContext('2d'); // On défini notre context 2D depuis notre carte 'card'
-       
-        Cards.push(bag.lstCards[cardId]);
+        // On ajoute un attribut perso pour identifier chaque canvas par l'identifiant de la carte
         card.setAttribute('card',i);
-        Cards[i].setCtx(ctx);
-        Cards[i].drawCard(image);
-        bag.removeBag(cardId); // On supprime cette carte du sac pour ne pas la repiocher une nouvlle fois
 
-        grille.appendChild(card); // On ajoute le canvas à notre page
+        // On défini le contexte pour pouvoir dessiner dans chaque canvas 
+        var ctx = card.getContext('2d'); 
+
+        // On génère un numéro aléatoire pour piocher la carte correspondante dans le sac
+        var cardId = rnd(0,bag.lstCards.length-1);
+       
+        // On ajoute au jeu la carte nouvellement tirée du sac
+        Cards.push(bag.lstCards[cardId]);
+        
+        // La carte reçoit son contexte pour qu'elle puisse se dessiner elle-même
+        Cards[i].setCtx(ctx);
+
+        // La carte se dessine
+        Cards[i].drawCard(image);
+
+        // On supprime cette carte du sac pour ne pas la repiocher une nouvelle fois
+        bag.removeBag(cardId);
+
+        // On ajoute le canvas à notre page
+        grille.appendChild(card);
+
+        // Création de l'événement "click" sur chaque canvas(carte) pour pouvoir appeller la fonction "flip" et ainsi retourner la carte
+        card.addEventListener('click',flip);
     }  
+    // Démmarrage du "tick" qui mettra à jour régulièrement le timer
     requestAnimationFrame(tick);
 }
 
 function flip() {
-    if(canPlay) {
+    if(canPlay) { // Si le joueur peut retourner la carte
+        // On récupère la carte dans la liste selon son attribut 'card' pour faire nos opérations dessus
         var card = Cards[this.getAttribute('card')];
+        // Si il y a moins de deux cartes sélectionnées, on peut en sélectonner une
         if(selectedCards.length !=2) {
-            if(card.getState() < 2) {
+            // Si la carte peut-être retournée
+            if(card.getState() == 0 ) {
+                // On l'ajoute a la liste des cartes sélectionnées
                 selectedCards.push(this.getAttribute('card'));
+                
+                // On passe l'état de la carte à "retournée"
                 card.setState(1);
-                card.drawCard(image)
+
+                // On redessine la carte retournée
+                card.drawCard(image);
+
+                // Si il y a deux cartes retournées, on peut les comparer
                 if(selectedCards.length == 2){
                     if(Cards[selectedCards[0]].getFruit() == Cards[selectedCards[1]].getFruit()) { // Les cartes sont identiques
+                        // On passe l'état des cartes à "validées"
                         Cards[selectedCards[0]].setState(2);
                         Cards[selectedCards[1]].setState(2);
+                        // On incrémente le nombre de paires trouvées pour la condition de victoire
                         pairesTrouvees += 1;
                     }else { // Les cartes sont différentes
+                        // On repasse les cartes en "non visible"
                         Cards[selectedCards[0]].setState(0);
                         Cards[selectedCards[1]].setState(0);
                     }
+                    // Un chronomètre pour redessiner automatiquement les cartes
                     setTimeout(function clear() {
+                        // On demande aux cartes sélectionnées de se redessiner
                         Cards[selectedCards[0]].drawCard();
                         Cards[selectedCards[1]].drawCard();
+                        // On vide le tableau des cartes sélectionnées
                         selectedCards = [];
                     },
                     timeToCheck * 1000);
+
+                    // Si le nombre de paires trouvées est égal au nombre total de paires, alors le joueur à gagné
                     if(pairesTrouvees == paires) {
+                        canPlay = false;
+                        // On appelle la fonction win() pour enregistrer le temps dans la base de données en lui passant le temps final
                         win(currentTime);
                     }
                 }
@@ -81,34 +131,54 @@ function flip() {
     }
 }
 
+
+/*
+  _____ ___ ____ _  __
+ |_   _|_ _/ ___| |/ /
+   | |  | | |   | ' / 
+   | |  | | |___| . \ 
+   |_| |___\____|_|\_\
+                      
+Cette fonction appellée automatiquement à chaque frame calculée par le navigateur (grâce à requestAnimationFrame)
+permet de gérer le timer, et la fin du jeu en cas de dépassement du temps défini.
+Pour prendre en compte toutes les configurations matérielles, nous devrions ajouter la gestion du DeltaTime (temps entre chaque frame calculée)
+pour avoir un lissage des valeurs, peut importe les performances de la machine sur laquelle s'execute l'application
+*/
+
 function tick() {
+    // Si le joueur peut jouer, on déroule le reste de la fonction
     if(canPlay) {
-        if(currentTime <= 0) {
-            console.warn('GAME OVER !');
+        if(currentTime <= 0) { // Le temps est dépassé, le jeu se termine par une défaite
+            alert('Game Over')
             canPlay = false;
-        }else {
-            requestAnimationFrame(tick);
-            currentTime -= 0.016 ;
+        }else { // Il reste du temps, le jeu continue 
+            // On décrémente le currentTime
+            currentTime -= 0.016 ; // 0.016 * 60fps est presque égal à 1 seconde, ici pour simplifier on ne prend pas en compte le DeltaTime
+
+            // On modifie la taille de la barre et le texte du chronomètre
             timerBar.style.width = ((currentTime/maxTime)*timerBarWidth) + "px";
             timerText.innerHTML = Math.ceil(currentTime);
-    
-            //$(".timer").progress((currentTime/maxTime)*timerBarWidth);
+            // On rappelle la fonction dans laquelle nous nous trouvons
+            requestAnimationFrame(tick);
         }
     }
 }
 
-/*(function ( $ ) {
-    $.fn.progress = function(pPourcent) {
-      var percent = pPourcent;
-      this.css("width", percent+"");
-      console.log(percent);
-    };
-}( jQuery ));*/
+/*
+  _____ _   _ ____  _____ ____ ___ ____ _____ ____  _____ __  __ _____ _   _ _____   ____  _   _   _____ _____ __  __ ____  ____  
+ | ____| \ | |  _ \| ____/ ___|_ _/ ___|_   _|  _ \| ____|  \/  | ____| \ | |_   _| |  _ \| | | | |_   _| ____|  \/  |  _ \/ ___| 
+ |  _| |  \| | |_) |  _|| |  _ | |\___ \ | | | |_) |  _| | |\/| |  _| |  \| | | |   | | | | | | |   | | |  _| | |\/| | |_) \___ \ 
+ | |___| |\  |  _ <| |__| |_| || | ___) || | |  _ <| |___| |  | | |___| |\  | | |   | |_| | |_| |   | | | |___| |  | |  __/ ___) |
+ |_____|_| \_|_| \_\_____\____|___|____/ |_| |_| \_\_____|_|  |_|_____|_| \_| |_|   |____/ \___/    |_| |_____|_|  |_|_|   |____/ 
+                                                                                                                                  
+*/
 
 function win(pTime) {
-    console.warn('BRAVO !');
-    canPlay = false;
+    alert('Bravo !');
+    // Pour enregistrer le temps passé à résoudre le jeu, on soustrait le temps écoulé en jeu au temps total
     var setTime = maxTime - pTime;
+
+    // On envoit le temps à enregistrer au script php chargé de l'enregistrement via une méthode POST
     jQuery.ajax({
         type: "POST",
         url: './libs/php/db.php',
